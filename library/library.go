4635680b08ae14030db17e83e044eaae0f15c74b
Fadjar <dark.fir21@gmail.com>
2021-05-29 18:46:23 +0700

    Tugas 6 GO

---
+++ library/library.go
@@ -0,0 +1,27 @@
+package library
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type mahasiswa struct {
+	nama string
+	umur int
+}
+
+func Tampildata() {
+	var n1, n2, n3 mahasiswa
+
+	n1.nama = "Ahmad"
+	n1.umur = rand.Intn(100)
+	n2.nama = "Abi"
+	n2.umur = rand.Intn(100)
+	n3.nama = "Zaka"
+	n3.umur = rand.Intn(100)
+
+	fmt.Println(n1.nama, n1.umur)
+	fmt.Println(n2.nama, n2.umur)
+	fmt.Println(n3.nama, n3.umur)
+
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "belajargolang/tugas_6/library"
+
+func main() {
+	library.Tampildata()
+}
